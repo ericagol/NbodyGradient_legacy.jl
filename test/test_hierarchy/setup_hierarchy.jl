e81a0fc4932e2c5a1ce31dc8bd8a3034a17bcec4
langfzac <langfzac@uw.edu>
2019-03-22 16:23:49 -0700

    Changed direction of vectors for hierarchy. (-1.0 <-> 1.0)

--- test/test_hierarchy/setup_hierarchy.jl
+++ test/test_hierarchy/setup_hierarchy.jl
@@ -24,7 +24,7 @@ function hierarchy(file::String)
 
     # Starts filling the array and returns it
     bottom_level(nbody,bins,h,1)
-    h[end,1:end] .= 1
+    h[end,1:end] .= -1
     return h
 end
 
@@ -45,7 +45,7 @@ function hierarchy(file::Array{Any,1})
 
     # Starts filling the array and returns it
     bottom_level(nbody,bins,h,1)
-    h[end,1:end] .= 1
+    h[end,1:end] .= -1
     return h
 end
 
@@ -55,16 +55,16 @@ end
 function bottom_level(nbody::Int64,bins::Array{Int64,2},h::Array{Float64},iter::Int64)
 
     # Fills the very first level of the hierarchy and iterates related variables
-    h[1,1] = 1
-    h[1,2] = -1
+    h[1,1] = -1.0
+    h[1,2] = 1.0
     if bins[1] > 1
         for i=1:bins[1]-1
             if !(@isdefined j) || isa(j,Nothing)
                 global j = i
             end
             if (j+3) <= nbody
-                h[i+1,j+2] = 1
-                h[i+1,j+3] = -1
+                h[i+1,j+2] = -1
+                h[i+1,j+3] = 1
             end
             j = +(j,2)
         end
@@ -79,7 +79,7 @@ function bottom_level(nbody::Int64,bins::Array{Int64,2},h::Array{Float64},iter::
     if 2*binsp == nbody
         symmetric(nbody,bodies,bins,binsp,row,h,iter+1)
     else
-        n_level(nbody,bodies,bins,binsp,row,h,iter+1)
+        nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
     end
 end
 
@@ -89,7 +89,7 @@ end
 # *** Only works for hierarchies with a max binaries per level of 2 (unless
 # symmetric). ***
 ################################################################################
-function n_level(nbody::Int64,bodies::Int64,bins::Array{Int64,2},binsp::Int64,row::Int64,h::Array{Float64},iter::Int64)
+function nlevel(nbody::Int64,bodies::Int64,bins::Array{Int64,2},binsp::Int64,row::Int64,h::Array{Float64},iter::Int64)
 #print("nbody: ", nbody, "\n")
 #print("bodies: ", bodies, "\n")
 #print("row: ", row, "\n")
@@ -104,61 +104,61 @@ if iter <= length(bins)
     elseif nbody == bodies
         if row == nbody-1
             if (bodies%4) > 2
-                h[row,1:row-2] .= 1
-                h[row,row-1:bodies] .= -1
+                h[row,1:row-2] .= -1
+                h[row,row-1:bodies] .= 1
                 return h
             elseif (bodies%4) <= 2
-                h[row,1:row-1] .= 1
-                h[row,row:bodies] .= -1
+                h[row,1:row-1] .= -1
+                h[row,row:bodies] .= 1
                 return h
             end
         end
     end
 
-    # Looks at the previous and current bins and calls n_level again
+    # Looks at the previous and current bins and calls nlevel again
     if nbody >= bodies
         if binsp == 1
             if bins[iter] == 1
-                h[row,1:bodies-binsp] .= 1
-                h[row,bodies-binsp + 1:bodies] .= -1
+                h[row,1:bodies-binsp] .= -1
+                h[row,bodies-binsp + 1:bodies] .= 1
                 row = row + binsp
                 binsp = bins[iter]
-                n_level(nbody,bodies,bins,binsp,row,h,iter+1)
+                nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             elseif bins[iter] == 2
-                h[row,1:bodies-(2*binsp)-1] .= 1
-                h[row,bodies-(2*binsp)] = -1
-                h[row+1,bodies-(2*binsp)+1] = 1
-                h[row+1,bodies] = -1
+                h[row,1:bodies-(2*binsp)-1] .= -1
+                h[row,bodies-(2*binsp)] = 1
+                h[row+1,bodies-(2*binsp)+1] = -1
+                h[row+1,bodies] = 1
                 row = row + 2
                 binsp = bins[iter]
-                n_level(nbody,bodies,bins,binsp,row,h,iter+1)
+                nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             end
         elseif binsp == 2
             if bins[iter] == 1
-                h[row,1:row-1] .= 1
-                h[row,row:bodies] .= -1
+                h[row,1:row-1] .= -1
+                h[row,row:bodies] .= 1
                 row = row + 1
                 binsp = bins[iter]
-                n_level(nbody,bodies,bins,binsp,row,h,iter+1)
+                nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             elseif bins[iter] == 2
-                h[row,1:row-1] .= 1
-                h[row,row:bodies-2*(binsp-1)] .= -1
-                h[row+1,bodies-2*(binsp-1)+1] = 1
-                h[row+1,bodies] = -1
+                h[row,1:row-1] .= -1
+                h[row,row:bodies-2*(binsp-1)] .= 1
+                h[row+1,bodies-2*(binsp-1)+1] = -1
+                h[row+1,bodies] = 1
                 row = row + 2
                 binsp = bins[iter]
-                n_level(nbody,bodies,bins,binsp,row,h,iter+1)
+                nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             end
         elseif binsp == 3
             if bins[iter] == 2
-                h[row,1:Int64(bodies/2)-1] .= 1
-                h[row,Int64(bodies/2):2*Int64(bodies/3)] .= -1
-                h[row+1,2*Int64(bodies/3)+1:bodies] .= 1
-                h[row+1,bodies+1] = -1
+                h[row,1:Int64(bodies/2)-1] .= -1
+                h[row,Int64(bodies/2):2*Int64(bodies/3)] .= 1
+                h[row+1,2*Int64(bodies/3)+1:bodies] .= -1
+                h[row+1,bodies+1] = 1
                 row = row + 2
                 binsp = bins[iter]
                 bodies = bodies + 1
-                n_level(nbody,bodies,bins,binsp,row,h,iter+1)
+                nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             end
         end
     end
@@ -172,12 +172,12 @@ end
 function symmetric(nbody::Int64,bodies::Int64,bins::Array{Int64,2},binsp::Int64,row::Int64,h::Array{Float64},iter::Int64)
     global j = 0
     while row < nbody-1
-        h[row,1+j:2+j] .= 1
-        h[row,j+3:j+4] .= -1
+        h[row,1+j:2+j] .= -1
+        h[row,j+3:j+4] .= 1
         j = j + 4
         row = row + 1
     end
-    h[row,1:binsp] .= 1
-    h[row,binsp+1:nbody] .= -1
+    h[row,1:binsp] .= -1
+    h[row,binsp+1:nbody] .= 1
     clear!(:j)
 end
