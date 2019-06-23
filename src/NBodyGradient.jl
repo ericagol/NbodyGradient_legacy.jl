1442c3f8bb2c01a92d8c75317dedd892d458409b
Zach Langford <zllangford@gmail.com>
2019-06-22 22:09:28 -0700

    Filled in usings and includes

--- src/NBodyGradient.jl
+++ src/NBodyGradient.jl
@@ -1,3 +1,23 @@
 module NBodyGradient
 
+    #################### Usings ####################
+	
+	using DelimitedFiles
+	using Distributed
+	using ForwardDiff
+	using LinearAlgebra
+
+    ################### Includes ###################
+	
+	include("g3.jl")
+	include("init_nbody.jl")
+	include("kepler.jl")
+	include("kepler_drift_solver.jl")
+	include("kepler_drift_step.jl")
+	include("kepler_init.jl")
+	include("kepler_solver_derivative.jl")
+	include("kelper_step.jl")
+	include("setup_hierarchy.jl")
+	include("ttv.jl")
+
 end
