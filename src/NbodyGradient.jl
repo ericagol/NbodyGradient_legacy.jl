7bd84742b278f32f09f0ea3bdc7de4edfb522fef
Zach Langford <zllangford@gmail.com>
2019-08-04 14:06:28 -0700

    Added exports for testing methods

--- src/NbodyGradient.jl
+++ src/NbodyGradient.jl
@@ -2,23 +2,29 @@ module NbodyGradient
 
     #################### Usings ####################
 	
-	using DelimitedFiles
-	using Distributed
-	using ForwardDiff
-	using LinearAlgebra
-	using Test
+		using DelimitedFiles
+		using Distributed
+		using ForwardDiff
+		using LinearAlgebra
+		using Test
 
     ################### Includes ###################
 	
-	include("g3.jl")
-	include("init_nbody.jl")
-	include("kepler.jl")
-	include("kepler_drift_solver.jl")
-	include("kepler_drift_step.jl")
-	include("kepler_init.jl")
-	include("kepler_solver_derivative.jl")
-	include("kepler_step.jl")
-	include("setup_hierarchy.jl")
-	include("ttv.jl")
+		include("g3.jl")
+		include("init_nbody.jl")
+		include("kepler.jl")
+		include("kepler_drift_solver.jl")
+		include("kepler_drift_step.jl")
+		include("kepler_init.jl")
+		include("kepler_solver_derivative.jl")
+		include("kepler_step.jl")
+		include("setup_hierarchy.jl")
+		include("ttv.jl")
+
+	################### Exports ###################
+
+		export ttv_elements!, ttv!
+		export findtransit2!, findtransit3!
+		export init_nbody, hierarchy
 
 end
