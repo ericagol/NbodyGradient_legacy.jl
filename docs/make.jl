2ad90442b96e31e62dbc58f229b2afc339688054
Zach Langford <langfzac@uw.edu>
2019-08-19 10:27:26 -0700

    Update make.jl
    Setup make.jl to create document site

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,20 @@ push!(LOAD_PATH,"../src/")
 using Documenter, NbodyGradient
 
 makedocs(
-    sitename="NbodyGradient",
+    sitename= "NbodyGradient.jl",
     pages = [
         "Home" => "index.md",
         "Installation" => "install.md",
-	"Initial Conditions" => "init.md"
+		"Initial Conditions" => "init.md"
     ],
     format = Documenter.HTML(
         prettyurls = false
     )
 )
-#=deploydocs(
+
+deploydocs(
     repo = "github.com/ericagol/NbodyGradient.jl.git",
-)=#
+	target = "build"
+	deps = nothing,
+	make = nothing
+)
